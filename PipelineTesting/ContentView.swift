2781556fad9d32fb11989d2543d5f30edd7f8a90
Alex Irimia <alexandru.irimia@dnata.com>
2023-08-31 15:06:59 +0400

    First commit

---
+++ PipelineTesting/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  PipelineTesting
+//
+//  Created by Alexandru Irimia on 31/08/2023.
+//
+
+import SwiftUI
+import SDWebImage
+import SwrveSDK
+import MapKit
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PipelineTesting/PipelineTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PipelineTestingApp.swift
+//  PipelineTesting
+//
+//  Created by Alexandru Irimia on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct PipelineTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
